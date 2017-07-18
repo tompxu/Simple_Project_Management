ff6065a5f926e02a605c54a38a68b34812d896c3
Tom Xu <pengxu.tom@gmail.com>
2017-07-18 10:09:08 -0400

    data connection need to fix (use OO)
    and no need to show html

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 session_start();
 
 $host = "localhost";
-$databasename = "phpproject";
+$databasename = "projecttrack";
 $databaseuser = "root";
-$databasepassword = "password";
+$databasepassword = "123456";
 
 mysql_connect($host,$databaseuser,$databasepassword);
 if ($ex != "no")
@@ -12,7 +12,3 @@ mysql_select_db($databasename);
 //
 $tablehead = '<table border="1" cellspacing="0" cellpadding="4" bordercolor="#999999">';
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-<link rel="stylesheet" href="style.css" type="text/css" />
